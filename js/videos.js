f1c82653c80b63741355c829fc29fd6a82feca58
Jp Robichaud <jp@rev.com>
2026-01-03 21:54:06 -0500

    Update videos from YouTube playlist

--- js/videos.js
+++ js/videos.js
@@ -1,6 +1,21 @@
 // Généré automatiquement par update-videos.sh
-// Fri 26 Dec 2025 09:07:30 PM EST
+// Sat 03 Jan 2026 09:53:53 PM EST
 
 const VIDEOS = [
+    { id: "sm0imq2IikA", title: "DSCN2815 ETUDESOM5" },
+    { id: "n0ATHnQH6z4", title: "DSCN2814 RIRE13" },
     { id: "rP1EA53lzc8", title: "DSCN2813 CLASSESOM3" },
+    { id: "-dmNtH1mVwo", title: "DSCN2823 EVENTMAINS22" },
+    { id: "3toFUk8ansE", title: "DSCN2821 EVENTMAINS18" },
+    { id: "9oQfEpVPnKE", title: "DSCN2817 EVENTAPPARITION17" },
+    { id: "JRK1dXY1LlU", title: "DSCN2810 EVENTSOM6" },
+    { id: "RBavG0SQyO0", title: "DSCN2835 ATTAQUE5" },
+    { id: "RkpKQFsgO2g", title: "DSCN2841 HALLUCGRP37" },
+    { id: "VHipPIGLJ-U", title: "DSCN2815 ETUDESOM5" },
+    { id: "YR9dNfZwyb8", title: "DSCN2834 ATTAQUE" },
+    { id: "fwEF1F5g-E8", title: "DSCN2829 EVENTHALLUC8" },
+    { id: "qe4Hw65dW4I", title: "DSCN2833 EVENTHALLUC10" },
+    { id: "rAFD9a0SCpQ", title: "DSCN2824 EVENTHALLUC3" },
+    { id: "slzNU0ERQik", title: "DSCN2837 EVENTSOM13" },
+    { id: "x5ztJGHRVPc", title: "DSCN2840 MORT S METIVIER" },
 ];
